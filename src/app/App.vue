ac07da54de5a57d51a555b9daaf79a77c7a3c757
Marvin Isaac <marvin@marvinisaac.com>
2021-01-02 06:40:54 +0800

    💄 Create basic layout of app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,17 +1,37 @@
 <template>
-    <button @click="ask()">
-        Ask
-    </button>
+    <div>
+        <textarea v-model="text"></textarea>
+    </div>
+    <div>
+        <button @click="ask()">
+            Ask
+        </button>
+    </div>
 </template>
 
 <script>
 import mythic from './services/mythic.js'
 
 export default {
+    data: () => ({
+        text: ''
+    }),
     methods: {
         ask() {
-            console.log(mythic.ask())
+            const result = mythic.ask()
+            this.text += '\r\n\r\n'
+            this.text += `> ${result.result}`
+            this.text += '\r\n\r\n'
         }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+textarea {
+    height: 50vh;
+    outline: none;
+    resize: none;
+    width: 50%;
+}
+</style>
